83ff1a834e16da8378eaeb878870dd954a71db92
Quentin ANIERE <quentin.aniere@gmail.com>
2023-01-14 11:43:16 -0500

    méchant goess brrrrrrrrr

--- src/main/java/codes/blitz/game/bot/Bot.java
+++ src/main/java/codes/blitz/game/bot/Bot.java
@@ -27,37 +27,39 @@ public class Bot
         Command command =  new Command();
 
         int i = 0;
-        int fric = (int) gameMessage.teamInfos().get(gameMessage.teamId()).money();
+        int sentNumber = 0;
 
-        System.out.println("début boucle argent");
-        while(fric >= 200) {
+        double fric = gameMessage.teamInfos().get(gameMessage.teamId()).money().doubleValue();
+
+        if(gameMessage.round().equals(1)) {
+            while (fric >= 15 && sentNumber < 8) {
+                command.addAction(new CommandActionSendReinforcements(EnemyType.LVL2, List.of(gameMessage.teams()).stream().filter(teamId -> !teamId.equals(gameMessage.teamId())).findFirst().orElseThrow()));
+                fric -= 15;
+                sentNumber++;
+            }
+        }
+
+        while (fric >= 200) {
 
             boolean placed = false;
 
-            System.out.println("Début boucle génération point");
-            while(!placed) {
+            while (!placed) {
                 int x = (int) (Math.random() * (gameMessage.map().width() - 1));
                 int y = (int) (Math.random() * (gameMessage.map().height() - 1));
                 Point newPoint = new Point(x, y);
 
-                System.out.println("Random point = "+ x + "-" + y);
-
-                if(gameMessage.playAreas().get(gameMessage.teamId()).grid().isEmpty(newPoint)) {
+                if (gameMessage.playAreas().get(gameMessage.teamId()).grid().isEmpty(newPoint)) {
                     command.addAction((new CommandActionBuild(TowerType.SPEAR_SHOOTER, newPoint)));
 
-                    System.out.println("Le nouveau point a été placé");
+                    System.out.println("New tower at " + x + "-" + y);
                     placed = true;
                     break;
 
-                } else {
-                    System.out.println("Génération d'un nouveau point...");
                 }
             }
-            System.out.println("Sortie boucle génération point");
 
             fric -= 200;
         }
-        System.out.println("Sortie boucle argent");
 
         // 3 possible commands: BUILD, SELL OR SEND_REINFORCEMENT ! Here are a few examples.
 //        command.addAction(new CommandActionBuild(TowerType.SPIKE_SHOOTER, new Point(10,10)));
