7a9350372a1563249d9878a4ecd0e0bc236ad481
Quentin ANIERE <quentin.aniere@gmail.com>
2023-01-14 12:49:55 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH

--- src/main/java/codes/blitz/game/bot/Bot.java
+++ src/main/java/codes/blitz/game/bot/Bot.java
@@ -7,12 +7,18 @@ import codes.blitz.game.message.game.GameMessage;
 import codes.blitz.game.message.game.commands.*;
 import codes.blitz.game.message.game.enemies.EnemyType;
 import codes.blitz.game.message.game.towers.TowerType;
+import codes.blitz.game.message.game.Path;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Bot
 {
+    private GameMap gameMap;
+    // map Distance 1 et 2
+    private int[][] mapD1, mapD2;
+
+    // Listes des truc a placer
+    ArrayList<Point> pufferSpot = new ArrayList<>();
     public Bot()
     {
         System.out.println("Papa ?");
@@ -25,6 +31,42 @@ public class Bot
     public Command getCommand(GameMessage gameMessage)
     {
         Command command =  new Command();
+
+        // get the map
+        if (gameMessage.round().intValue() == 0)
+        {
+            // get the original map
+            gameMap = gameMessage.map();
+            // Calculer D1 et D2
+            getMap();
+
+            // Calculer tous les nombre de chemin de 3
+
+            LinkedHashMap<Integer, Point> pufferSpotMap = new LinkedHashMap<>();
+
+            for (int i = 0; i < gameMap.height(); i++)
+            {
+                for (int j = 0; j < gameMap.width(); j++)
+                {
+                    if (mapD1[i][j] >= 2)
+                    {
+                        pufferSpotMap.put(mapD1[i][j], new Point(j, i));
+                    }
+                }
+            }
+
+
+
+            Map<Integer, Point> prout = new TreeMap<>(pufferSpotMap);
+            for(Integer score: prout.keySet()) {
+
+                System.out.println(pufferSpot + " = " + score);
+
+                pufferSpot.add(pufferSpotMap.get(score));
+            }
+        }
+
+        // Placer les unités sur les troisChemin
 
         int i = 0;
         int sentNumber = 0;
@@ -45,6 +87,12 @@ public class Bot
                 fric -= 120;
                 sentNumber++;
             }
+        }
+
+        while(!pufferSpot.isEmpty() && fric >= 280) {
+            Point newPoint = pufferSpot.get(0);
+            command.addAction((new CommandActionBuild(TowerType.SPIKE_SHOOTER, newPoint)));
+            pufferSpot.remove(0);
         }
 
         while (fric >= 200) {
@@ -76,5 +124,85 @@ public class Bot
 //        command.addAction(new CommandActionSendReinforcements(EnemyType.LVL1,
 //                List.of(gameMessage.teams()).stream().filter(teamId -> !teamId.equals(gameMessage.teamId())).findFirst().orElseThrow()));
         return command;
+    }
+
+    private void getMap()
+    {
+        // calculate the path
+        Path[] path = gameMap.paths();
+
+        // width = y et height = x
+        int[][] map = new int[gameMap.height()][gameMap.width()];
+        mapD1 = new int[gameMap.height()][gameMap.width()];
+        mapD2 = new int[gameMap.height()][gameMap.width()];
+
+        // initialize la map a zero
+        for (int i = 0; i < gameMap.height(); i++)
+        {
+            for (int j = 0; j < gameMap.width(); j++)
+            {
+                map[i][j] = 0;
+                mapD1[i][j] = 0;
+                mapD2[i][j] = 0;
+            }
+        }
+
+        // Ajouter les chemin comme -1
+        for (int i = 0; i < path.length; i++) {
+            for (int j = 0; j < path[i].tiles().length; j++) {
+                // le point du chemin
+                Point point = path[i].tiles()[j];
+                map[point.y()][point.x()] = -1;
+                mapD1[point.y()][point.x()] = -1;
+                mapD2[point.y()][point.x()] = -1;
+            }
+        }
+
+        // On itterre sur la map pour calculer le nombre de tuiles du chemin accessible depuis chaque tuile
+        for (int i = 0; i < gameMap.height(); i++)
+        {
+            for (int j = 0; j < gameMap.width(); j++)
+            {
+                // Si la tuile n'est pas un chemin
+                if (map[i][j] != -1)
+                {
+                    // On itterre sur les tuiles adjacentes avec D1
+                    for (int k = i - 1; k <= i + 1; k++)
+                    {
+                        for (int l = j - 1; l <= j + 1; l++)
+                        {
+                            // Si la tuile est dans la map
+                            if (k >= 0 && k < gameMap.height() && l >= 0 && l < gameMap.width())
+                            {
+                                // Si la tuile est un chemin
+                                if (map[k][l] == -1)
+                                {
+                                    // On incremente le nombre de chemin accessible
+                                    mapD1[i][j]++;
+                                }
+                            }
+                        }
+                    }
+
+                    // On itterre sur les tuiles adjacentes avec D2
+                    for (int k = i - 2; k <= i + 2; k++)
+                    {
+                        for (int l = j - 2; l <= j + 2; l++)
+                        {
+                            // Si la tuile est dans la map
+                            if (k >= 0 && k < gameMap.height() && l >= 0 && l < gameMap.width())
+                            {
+                                // Si la tuile est un chemin
+                                if (map[k][l] == -1)
+                                {
+                                    // On incremente le nombre de chemin accessible
+                                    mapD2[i][j]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
