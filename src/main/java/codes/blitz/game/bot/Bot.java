b9856501087fec92d91cb70eac3f7b87ca720e0f
Luc <wolf-361@hotmail.com>
2023-01-14 09:19:07 -0500

    test1 met des tour sur les tuiles libre si on a le cash
    v5

--- src/main/java/codes/blitz/game/bot/Bot.java
+++ src/main/java/codes/blitz/game/bot/Bot.java
@@ -17,6 +17,11 @@ public class Bot
     {
         System.out.println("Initializing your super duper mega bot.");
         // initialize some variables you will need throughout the game here
+
+        // get the game state and the list of free tiles
+        GameMessage gameMessage = GameMessage.getGameMessage();
+        List<Point> freeTiles = gameMessage.getFreeTiles();
+
     }
 
     /*
@@ -27,8 +32,14 @@ public class Bot
         Command command =  new Command();
 
         // 3 possible commands: BUILD, SELL OR SEND_REINFORCEMENT ! Here are a few examples.
-        command.addAction(new CommandActionBuild(TowerType.SPIKE_SHOOTER, new Point(10,10)));
-        command.addAction(new CommandActionSell(new Point(10,10)));
+
+        // build a tower on a free tile (if you have enough money)
+        if (gameMessage.getMoney() >= TowerType.TOWER_1.getCost())
+        {
+            command.setAction(new CommandActionBuild(TowerType.TOWER_1, gameMessage.getFreeTiles().get(0)));
+        }
+
+        // Send a reinforcement to the enemy
         command.addAction(new CommandActionSendReinforcements(EnemyType.LVL1,
                 List.of(gameMessage.teams()).stream().filter(teamId -> !teamId.equals(gameMessage.teamId())).findFirst().orElseThrow()));
 
